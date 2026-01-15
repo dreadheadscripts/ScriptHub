4127edefd523ee287a6353a33d50202d957dcd49
dreadheadscripts <decoreyhardy0@gmail.com>
2026-01-14 21:49:39 -0600

    Update Final-KeySystem.lua

--- Key/Final-KeySystem.lua
+++ Key/Final-KeySystem.lua
@@ -1,6 +1,8 @@
 -- Define your main script as a global string
 _G.MainScript = [[
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/dreadheadscripts/ScriptHub/refs/heads/main/Aimbot-Script/Base-Gui.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dreadheadscripts/ScriptHub/refs/heads/main/Aimbot-Script/Base-Gui.lua"))()
+
 ]]
 -- Key System Config
 -- Load and run the key system script after setting MainScript
